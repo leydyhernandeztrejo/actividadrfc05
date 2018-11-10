38d12a5bb1645d928d49f9ea264efc584a59d7ed
leydyhernandeztrejo <39417008+leydyhernandeztrejo@users.noreply.github.com>
2018-11-09 23:25:32 -0600

    terminado

---
+++ CalculaRFC/src/main/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+import views.ViewRfc;
+import models.ModelRfc;
+import controllers.ControllerRfc;
+/**
+ *
+ * @author morenita
+ */
+public class Main {
+    private static ViewRfc viewRfc;
+    private static ModelRfc modelRfc;
+    private static ControllerRfc controllerRfc;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        viewRfc = new ViewRfc();
+        modelRfc = new ModelRfc();
+        controllerRfc = new ControllerRfc(viewRfc, modelRfc);
+    }
+    
+}
---
+++ CalculaRFC/src/models/ModelRfc.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author morenita
+ */
+//variables
+public class ModelRfc {
+    Scanner leer = new Scanner(System.in);
+        
+    String nombre, paterno, materno, anio, mes, dia, rfc;
+
+    public Scanner getLeer() {
+        return leer;
+    }
+
+    public void setLeer(Scanner leer) {
+        this.leer = leer;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaterno() {
+        return paterno;
+    }
+
+    public void setPaterno(String paterno) {
+        this.paterno = paterno;
+    }
+
+    public String getMaterno() {
+        return materno;
+    }
+
+    public void setMaterno(String materno) {
+        this.materno = materno;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public void setAnio(String año) {
+        this.anio = anio;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+    
+}
